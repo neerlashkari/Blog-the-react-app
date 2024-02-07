61ce99779d91762d8c846ce8e648ed0cc4283188
Neer Lashkari <103020460+neerlashkari@users.noreply.github.com>
2024-02-07 17:29:20 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="../src/assets/logo.webp" alt="" />
+    </div>
+  )
+}
+
+export default Logo
